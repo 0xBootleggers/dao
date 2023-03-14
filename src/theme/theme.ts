4415084c97fe193633338330058c3614f647d9e0
skuhlmann <samkuhlmann@gmail.com>
2023-03-14 16:59:52 -0600

    more theme testing

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,20 @@
+import { defaultDarkTheme } from "@daohaus/ui";
+
+const greyBoots = {
+  step1: "#fcfcfc",
+  step2: "#f8f8f8",
+  step3: "#f3f3f3",
+  step4: "#ededed",
+  step5: "#e8e8e8",
+  step6: "#e2e2e2",
+  step7: "#dbdbdb",
+  step8: "#c7c7c7",
+  step9: "#8f8f8f",
+  step10: "#858585",
+  step11: "#6f6f6f",
+  step12: "#171717",
+};
+
 export const BOOTLEGGERS_THEME = {
   themeName: "bootleggers",
   rootBgColor: "#000000",
@@ -7,6 +24,16 @@ export const BOOTLEGGERS_THEME = {
     family: {
       body: `'Alegreya', serif`,
       data: `'Space Mono', monospace`,
+    },
+  },
+  button: {
+    ...defaultDarkTheme.button,
+    primary: {
+      ...defaultDarkTheme.button.primary,
+    },
+    secondary: {
+      ...defaultDarkTheme.button.secondary,
+      text: "pink",
     },
   },
   primary: {
@@ -23,34 +50,15 @@ export const BOOTLEGGERS_THEME = {
     step11: "white",
     step12: "white",
   },
-  //   secondary: {
-  //     step1: "black",
-  //     step2: "black",
-  //     step3: "black",
-  //     step4: "black",
-  //     step5: "black",
-  //     step6: "black",
-  //     step7: "black",
-  //     step8: "black",
-  //     step9: "black",
-  //     step10: "black",
-  //     step11: "black",
-  //     step12: "black",
-  //   },
+  secondary: {
+    ...defaultDarkTheme.secondary,
+    step2: "#3d3d3d",
+    step5: greyBoots.step5,
+    step9: "white",
+    step3: greyBoots.step11,
+    step10: greyBoots.step10,
+    step11: greyBoots.step1,
+  },
 };
-// font-family: 'Alegreya', serif;
 
-// const bootLeggerDark = {
-//     step1: amberDark.amber1,
-//     step2: amberDark.amber2,
-//     step3: amberDark.amber3,
-//     step4: amberDark.amber4,
-//     step5: amberDark.amber5,
-//     step6: amberDark.amber6,
-//     step7: amberDark.amber7,
-//     step8: amberDark.amber8,
-//     step9: amberDark.amber9,
-//     step10: amberDark.amber10,
-//     step11: amberDark.amber11,
-//     step12: amberDark.amber12,
-//   };
+defaultDarkTheme;
